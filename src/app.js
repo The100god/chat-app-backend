38255b7a10f6911f652e2a3d5d41504d5fbf3750
saurabh goyal <89391860+The100god@users.noreply.github.com>
2026-07-12 20:44:01 +0530

    updated ui and login issue

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ app.use(cookieParser());
 // middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
